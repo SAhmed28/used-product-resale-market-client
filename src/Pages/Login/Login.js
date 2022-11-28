52c4e07b4ff8c689aec68ddc71dfb09678d2a490
SAhmed28 <sanjida1@ualberta.ca>
2022-11-28 04:18:18 -0600

    Login page design and hook implemented

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
+import { FaGoogle } from "react-icons/fa";
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [loginError, setLoginError] = useState('');
+
+    const handleLogin = (data) => {
+        console.log(data);
+        setLoginError('');
+
+        // signIn(data.email, data.password)
+        //     .then(res => {
+        //         const user = res.user;
+        //         console.log(user);
+        //         setLoginUserEmail(data.email);
+        //     })
+        //     .catch(err => {
+        //         console.error(err);
+        //         setLoginError(err.message);
+        //     })
+    }
     return (
-        <div>
-            <h2>Login page</h2>
+        <div className='w-[450px] mx-auto shadow-xl p-7 my-4'>
+            <h2 className='text-2xl text-center text-primary font-bold'>Login</h2>
+
+            <form onSubmit={handleSubmit(handleLogin)}>
+
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" {...register("email", { required: "Email Address is required" })} className="input input-bordered w-full" />
+                    {errors.email && <p className='text-error'>{errors.email?.message}</p>}
+                </div>
+
+
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password"
+                        {...register("password", {
+                            required: "Password is required",
+                            minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                        })}
+                        className="input input-bordered w-full" />
+
+                    {errors.password && <p className='text-error'>{errors.password?.message}</p>}
+
+                    <label className="label">
+                        <Link className="label-text-alt">Forgot Password?</Link>
+                    </label>
+                </div>
+
+                <input type="submit" className='btn btn-primary text-white w-full mt-9 mb-3' value='Login' />
+                <div>
+                    {loginError && <p className='text-error'>{loginError}</p>}
+                </div>
+
+            </form>
+
+            <p className='text-center'>New to Smart Tech? <Link to='/signup' className='text-secondary'>Create new account</Link></p>
+
+            <div>
+                <div className="divider mt-4">OR</div>
+                <button className='btn btn-outline btn-secondary w-full mt-4 mb-3'> <FaGoogle/> <span className='mx-4'>Continue with google</span></button>
+            </div>
         </div>
     );
 };
