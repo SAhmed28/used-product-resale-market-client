e2bbcb9b7b8c40e390e182bce292fe3a65eb050b
SAhmed28 <sanjida1@ualberta.ca>
2022-11-28 11:07:47 -0600

    My products page done almost

--- src/Pages/Dashboard/AddProducts/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/AddProducts/MyProducts/MyProducts.js
@@ -2,11 +2,12 @@ import React, { useContext } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import Loading from '../../../Shared/Loading/Loading';
 import { AuthContext } from '../../../../contexts/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const MyProducts = () => {
     const {user} = useContext(AuthContext)
     const { data: myProducts = [], refetch, isLoading } = useQuery({
-        queryKey: ['availableOptions'],
+        queryKey: ['myproducts', user?.email],
         queryFn: async () => {
             const res = await fetch(`http://localhost:5000/products?email=${user?.email}`);
             const data = await res.json();
@@ -22,7 +23,44 @@ const MyProducts = () => {
 
     return (
         <div>
-            <h2>This is my product</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Product Name</th>
+                            <th>Re-sale Price</th>
+                            <th>Date Posted</th>
+                            <th>Payment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myProducts &&
+                            myProducts?.map((myProduct, i) => <tr key={myProduct._id}>
+                                <th>{i+1}</th>
+                                <td>{myProduct.productName}</td>
+                                <td>{myProduct.resalePrice}</td>
+                                <td>{myProduct.date}</td>
+                                <td>
+                                    {
+                                        myProduct?.resalePrice && !myProduct.paid &&
+                                        <Link to={`/dashboard/payment/${myProduct._id}`}>
+                                            <button className='btn btn-primary btn-sm'>Pay</button>
+                                        </Link>
+                                    }
+                                    {
+                                        myProduct.price && myProduct.paid && <span className='text-secondary font-bold'>Paid</span>
+                                    }
+                                </td>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
